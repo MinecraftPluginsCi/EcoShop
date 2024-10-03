8b96fc01b6476f803f55eee25559b2f65082d06c
qscbm187531 <1309635304@qq.com>
2024-10-03 12:20:12 +0800

    Update build.gradle.kts

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -5,31 +5,6 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            version = project.version.toString()
-            artifactId = rootProject.name
-
-            artifact(rootProject.tasks.shadowJar.get().archiveFile)
-        }
-    }
-
-    publishing {
-        repositories {
-            maven {
-                name = "auxilor"
-                url = uri("https://repo.auxilor.io/repository/maven-releases/")
-                credentials {
-                    username = System.getenv("MAVEN_USERNAME")
-                    password = System.getenv("MAVEN_PASSWORD")
-                }
-            }
-        }
-    }
-}
-
 tasks {
     build {
         dependsOn(publishToMavenLocal)
